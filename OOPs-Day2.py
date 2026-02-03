f3a701d0fdd1019362076e624a1c7a4b375b2374
PRATHAM-p25 <148321716+PRATHAM-p25@users.noreply.github.com>
2026-02-03 12:44:31 +0530

    Enhance Instagram class with comments and likes features
    Refactor Instagram class methods and add comment management.

--- OOPs-Day2.py
+++ OOPs-Day2.py
@@ -1,66 +1,80 @@
-# For the Given code add the comments, Creator_name, Location and their display methods . 
-# Note comments should be a list of strings. 
-
-# Add a delete method to delete the last comments when ever that method is 
-# called with respective object.
-
-# Class and object
-
-
 class Instagram:
-    def __init__(self,title, description,creator_name,location):    
+    def __init__(self, title, description, creator_name, location):
         self.title = title
         self.description = description
-        self.likes = 0
-        self.creator_name = creator_name 
+        self.creator_name = creator_name
         self.location = location
-        self.comments = [] 
+        self.likes = 0
+        self.comments = []
+
     def display_title(self):
-        print("The title of the reel is ",self.title)
+        print("The title of the reel is ", self.title)
     def display_description(self):
-        print("The description of the reel is ",self.description)
-    def display_likes(self):
-        print("The likes of the reel is ",self.likes)
-    def liked(self):
-        self.likes += 1
-    def disliked(self):
-        if self.likes > 0:
-            self.likes-=1 
-    def display_creator_name(self):
-        print("The creator name is ",self.creator_name) 
+        print("The description of the reel is ", self.description)
+    def display_creator(self):
+        print("The creator of the reel is ", self.creator_name)
     def display_location(self):
-        print("The location is ",self.location)
-    def display_comment(self):
+        print("The location of the reel is ", self.location)
+    def display_likes(self):
+        print("The likes of the reel is ", self.likes)
+    def display_comments(self):
         if len(self.comments) == 0:
             print("No comments yet")
         else:
             print("The comments are ")
             for comment in self.comments: 
                 print("-",comment)
-    def add_comments(self,comment):
+    def liked(self):
+        self.likes += 1
+
+    def disliked(self):
+        if self.likes > 0:
+            self.likes -= 1
+
+    def add_comment(self, comment):
         self.comments.append(comment)
-    def delete_last_comment(self):
-        temp_comment=self.comments.pop()
-        print("The last comment is deleted ",temp_comment)
+
+    def remove_last_comment(self):
+        if self.comments:
+            self.comments.pop()
 
 
-        
+reel1 = Instagram("dancing", "dancing with friends", "XYZ", "Mysore")
+
+reel1.display_title()
+reel1.display_description()
+reel1.display_creator()
+reel1.display_location()
+reel1.liked()
+reel1.liked()
+reel1.display_likes()
+reel1.add_comment("Nice dance")
+reel1.add_comment("Awesome")
+reel1.display_comments()
+reel1.disliked()
+reel1.display_likes()
 
 
-reel1=Instagram("dancing","dancing with friends","John","New York")
-# comment=[]
+reel2 = Instagram("finance minister conference", "finance minister conference with friends", "ABCD", "Delhi")
 
-reel1.add_comments("comment1")
-reel1.display_comment()
-reel1.delete_last_comment()
-reel1.display_comment()
+reel2.display_title()
+reel2.display_description()
+reel2.display_creator()
+reel2.display_location()
+reel2.liked()
+reel2.liked()
+reel2.liked()
+reel2.display_likes()
+reel2.add_comment("Informative")
+reel2.display_comments()
+reel2.disliked()
+reel2.display_likes()
 
 
+print(id(reel1))
+print(id(reel2))
+
+reel1.remove_last_comment()
+reel1.display_comments()
 
 
-# reel2=Instagram("finance minister conference","finance minister conference with friends","Jane","London") 
-# reel1.display_title()
-# reel1.display_description()
-# reel1.display_creator_name()
-# reel1.display_location()
-# reel1.display_comment()
